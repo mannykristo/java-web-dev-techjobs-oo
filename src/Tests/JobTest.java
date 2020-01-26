96bbeb0a50c1927e801ef0f096921cddd6599b1a
Manuel Kristo <manny.kristo@gmail.com>
2020-01-26 15:57:38 -0600

    declared and initialized two Job objects

---
+++ src/Tests/JobTest.java
@@ -0,0 +1,27 @@
+package Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.Job;
+
+import static org.junit.Assert.assertEquals;
+
+public class JobTest {
+
+    Job fakeJob;
+    Job fakeJob2;
+
+    @Before
+    public void testJobObjects() {
+        fakeJob = new Job();
+        fakeJob2 = new Job();
+    }
+
+    @Test
+    public void testSettingJobId() {
+        assertEquals();
+    }
+
+
+
+}
