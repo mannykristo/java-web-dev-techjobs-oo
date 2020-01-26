e40494dd0560b458973e476eef7ecccb2f1bb8cc
Manuel Kristo <manny.kristo@gmail.com>
2020-01-26 16:26:12 -0600

    Test full constructor

--- src/Tests/JobTest.java
+++ src/Tests/JobTest.java
@@ -2,9 +2,11 @@ package Tests;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.launchcode.techjobs_oo.Job;
+import org.launchcode.techjobs_oo.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 
 public class JobTest {
 
@@ -19,9 +21,28 @@ public class JobTest {
 
     @Test
     public void testSettingJobId() {
-        assertEquals();
+        assertEquals(fakeJob, fakeJob2, 1);
+
+
     }
 
+    private void assertEquals(Job fakeJob, Job fakeJob2, int i) {
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue("Product tester".equals(testJob.getName()));
+        assertTrue(testJob.getEmployer() instanceof Employer);
+        assertTrue(testJob.getLocation() instanceof Location);
+        assertTrue(testJob.getPositionType() instanceof PositionType);
+        assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
+    }
+
+    @Test
+    public void testJobsForEquality() {
+
+    }
 
 
 }
