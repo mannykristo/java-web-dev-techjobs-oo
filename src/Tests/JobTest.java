da5a755b3e44d6b4d7f6085a274a55b03dabde2a
Manuel Kristo <manny.kristo@gmail.com>
2020-01-26 20:18:49 -0600

    Finishing up step 5, organized initialization in JobTest. Starting to create toString method in job class

--- src/Tests/JobTest.java
+++ src/Tests/JobTest.java
@@ -10,41 +10,54 @@ import static org.junit.Assert.*;
 public class JobTest {
 
     Job fakeJob;
+    Job fakeJob1;
     Job fakeJob2;
+    Job fakeJob3;
+    Job fakeJob4;
 
     @Before
     public void testJobObjects() {
         fakeJob = new Job();
-        fakeJob2 = new Job();
-    }
+        fakeJob1 = new Job();
+        fakeJob2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        fakeJob3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        fakeJob4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    };
 
     @Test
     public void testSettingJobId() {
-        assertEquals(fakeJob, fakeJob2, 1);
+        assertEquals(fakeJob, fakeJob1, 1);
+    };
 
-
-    }
-
-    private void assertEquals(Job fakeJob, Job fakeJob2, int i) {
-    }
+    private void assertEquals(Job fakeJob, Job fakeJob1, int i) {};
 
     @Test
     public void testJobConstructorSetsAllFields() {
-        Job testJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        assertTrue("Product tester".equals(testJob.getName()));
-        assertTrue(testJob.getEmployer() instanceof Employer);
-        assertTrue(testJob.getLocation() instanceof Location);
-        assertTrue(testJob.getPositionType() instanceof PositionType);
-        assertTrue(testJob.getCoreCompetency() instanceof CoreCompetency);
-    }
+        assertTrue("Product tester".equals(fakeJob3.getName()));
+        assertTrue(fakeJob3.getEmployer() instanceof Employer);
+        assertTrue(fakeJob3.getLocation() instanceof Location);
+        assertTrue(fakeJob3.getPositionType() instanceof PositionType);
+        assertTrue(fakeJob3.getCoreCompetency() instanceof CoreCompetency);
+    };
 
     @Test
     public void testJobsForEquality() {
-       Job fakeJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-       Job fakeJob2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertFalse(fakeJob.equals(fakeJob1));
+    };
 
-        assertFalse(fakeJob.getId()==fakeJob2.getId());
-    }
+   @Test
+   public String toString() {
+       String toStringTest = "\n" +
+               "ID: " + fakeJob4.getId() + "\n" +
+               "Name: " + fakeJob4.getName() + "\n" +
+               "Employer: " + fakeJob4.getEmployer() + "\n" +
+               "Location: " + fakeJob4.getLocation() + "\n" +
+               "Position Type: " + fakeJob4.getPositionType() + "\n" +
+               "Core Competency: " + fakeJob4.getCoreCompetency() + "\n";
+               return toStringTest;
+    };
 
-
-}
+};
